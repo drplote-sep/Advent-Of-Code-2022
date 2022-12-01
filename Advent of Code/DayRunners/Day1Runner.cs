59a1d52d8d8bd373a2b0a13404fc3afd44d2045f
Derek Plote <drplote@sep.com>
2022-12-01 11:02:07 -0500

    Day 1

--- Advent of Code/DayRunners/Day1Runner.cs
+++ Advent of Code/DayRunners/Day1Runner.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Advent_of_Code.DataSources;
 
@@ -12,8 +13,45 @@ namespace Advent_of_Code.DayRunners
 
         protected override void SolveDay(string[] data)
         {
-            OutputWriter.WriteResult(1, $"MY BODY");
-            OutputWriter.WriteResult(2, $"IS READY");
+            var elves = GetElves(data);
+
+            var orderedElves = elves.OrderByDescending(e => e.TotalCalories);
+            OutputWriter.WriteResult(1, $"The highest total calories for an elf is {orderedElves.First().TotalCalories}");
+
+            var top3 = orderedElves.Take(3);
+            OutputWriter.WriteResult(2, $"The total calories for the top 3 elves is {top3.Select(e => e.TotalCalories).Sum()}");
+        }
+
+        private List<Elf> GetElves(string[] data)
+        {
+            var elves = new List<Elf>();
+            var currentElf = new Elf();
+            elves.Add(currentElf);
+            foreach (var line in data)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    currentElf = new Elf();
+                    elves.Add(currentElf);
+                }
+                else
+                {
+                    currentElf.Calories.Add(Convert.ToInt32(line));
+                }
+            }
+            
+            return elves;
+        }
+    }
+
+    public class Elf
+    {
+        public List<int> Calories { get; private set; } = new List<int>();
+        
+        public int TotalCalories => Calories.Sum();
+
+        public Elf()
+        {
         }
     }
 }
--- Advent of Code/Program.cs
+++ Advent of Code/Program.cs
@@ -9,13 +9,15 @@ namespace Advent_of_Code
 {
     internal class Day1Program
     {
+        private static readonly bool ShouldUseTestData = false;
+        
         private static void Main(string[] args)
         {
             OutputWriter.WriteHeader("Advent of Code 2022");
-
+            
             foreach (var dayRunner in GetDayRunners(1))
             {
-                dayRunner.Go(true);
+                dayRunner.Go(ShouldUseTestData);
             }
         }
 
